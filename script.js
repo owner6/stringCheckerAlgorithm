98f9350b4ed5fe38b352a0b1f2bfc23bb1a2c761
owner6 <owner1147@gmail.com>
2023-08-03 22:17:01 +0300

    correction of left indents

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
 const stringChecker = function(s, p1, p2) {
-  n = 0;
-  subStr1 = 0;
-  subStr2 = 0;
+  let n = 0;
+  let subStr1 = 0;
+  let subStr2 = 0;
 
   while (n < s.length) {
     if (subStr1 < p1.length && p1[subStr1] === s[n]) {
-      subStr1++
-  } else if (subStr2 < p2.length && p2[subStr2] === s[n]) {
-      subStr2++
-  } else {
+      subStr1++;
+    } else if (subStr2 < p2.length && p2[subStr2] === s[n]) {
+      subStr2++;
+    } else {
       return false;
-    } 
-  n++
+    }
+    n++;
   }
-  return subStr1 === p1.length && subStr2 === p2.length 
-}
+  
+  return subStr1 === p1.length && subStr2 === p2.length;
+};
 
-console.log(stringChecker('radency', 'rdnc', 'aey'))
+console.log(stringChecker('radency', 'rdnc', 'aey'));
